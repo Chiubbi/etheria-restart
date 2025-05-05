fa777247dbb95a7390ede082d9fd4c82a63211cb
Chiubbi <chiubbicontact@gmail.com>
2025-05-05 22:32:12 +0200

    Create personnages.js

---
+++ pages/personnages.js
@@ -0,0 +1,18 @@
+import Layout from '../components/layout';
+
+const Personnages = () => {
+  return (
+    <Layout>
+      <h1>Les Personnages d'Etheria Restart</h1>
+      <p>Voici la liste des personnages disponibles :</p>
+      <ul>
+        <li><strong>Dokidoki</strong> – Meilleur soigneur</li>
+        <li><strong>Lily</strong> – Puissante en soutien</li>
+        <li><strong>Heinrich</strong> – Fort en PVE</li>
+        <li><strong>Lingluo</strong> – Soigneur et débuffeur</li>
+      </ul>
+    </Layout>
+  );
+};
+
+export default Personnages;
