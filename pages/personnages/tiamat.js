7f6cbb776c5f19f654a00cf7912b0e93b0951e1d
Chiubbi <chiubbicontact@gmail.com>
2025-05-05 22:54:59 +0200

    Update tiamat.js

--- pages/personnages/tiamat.js
+++ pages/personnages/tiamat.js
@@ -10,17 +10,18 @@ const Tiamat = () => {
         alt="Tiamat"
         style={{ width: '300px', height: 'auto', borderRadius: '8px' }}
       />
+      
       <h2>Introduction</h2>
       <p>
-        Tiamat is a SSR character from the Odd Odd element who is part of the Leiboz Life faction. The character is also known by the Abyssal Nightmare title.
+        Tiamat est un personnage SSR de l'élément Odd Odd, appartenant à la faction Leiboz Life. Ce personnage est également connu sous le titre d'Abyssal Nightmare.
       </p>
 
-      <h3>Profile</h3>
+      <h3>Profil</h3>
       <p>
-        To learn more about Tiamat check the sections below. Use the tabs to quickly switch to the kind of information you're looking for.
+        Pour en savoir plus sur Tiamat, consultez les sections ci-dessous. Utilisez les onglets pour passer rapidement au type d'information que vous recherchez.
       </p>
 
-      <h3>Stats (Lvl 50)</h3>
+      <h3>Statistiques (Niveau 50)</h3>
       <ul>
         <li>HP: 22815</li>
         <li>DEF: 830</li>
@@ -32,19 +33,19 @@ const Tiamat = () => {
         <li>Effect RES: 20%</li>
       </ul>
 
-      <h3>Skills</h3>
-      <p>Here are Tiamat's skills:</p>
+      <h3>Compétences</h3>
+      <p>Voici les compétences de Tiamat :</p>
       <ul>
-        <li><strong>Skill 1:</strong> Thunder Spear</li>
-        <li><strong>Skill 2:</strong> Seal of Life</li>
-        <li><strong>Skill 3:</strong> Eternity</li>
-        <li><strong>Skill 1 (Alt):</strong> Purge Day</li>
+        <li><strong>Compétence 1:</strong> Thunder Spear</li>
+        <li><strong>Compétence 2:</strong> Seal of Life</li>
+        <li><strong>Compétence 3:</strong> Eternity</li>
+        <li><strong>Compétence 1 (Alt):</strong> Purge Day</li>
       </ul>
 
       <h3>Dupes</h3>
-      <p>Tiamat dupes information aren't available yet. They will be added soon!</p>
+      <p>Les informations sur les dupes de Tiamat ne sont pas encore disponibles. Elles seront ajoutées bientôt !</p>
 
-      {/* Ajoute un lien pour revenir à la page principale des personnages */}
+      {/* Lien pour revenir à la page principale des personnages */}
       <Link href="/personnages">
         <a style={{ color: 'blue' }}>Retour à la liste des personnages</a>
       </Link>
