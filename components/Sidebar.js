ab584697d0b357d35b465f28ad15612a7bc0de67
Chiubbi <chiubbicontact@gmail.com>
2025-05-05 23:12:29 +0200

    Update Sidebar.js

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,4 +1,3 @@
-// components/Sidebar.js
 import Link from 'next/link';
 
 const Sidebar = () => {
@@ -7,11 +6,16 @@ const Sidebar = () => {
       <h2>Menu</h2>
       <ul>
         <li>
+          <Link href="/">
+            <a>Accueil</a>
+          </Link>
+        </li>
+        <li>
           <Link href="/personnages">
             <a>Personnages</a>
           </Link>
         </li>
-        {/* Ajoute d'autres liens ici */}
+        {/* Ajoute d'autres liens si nécessaire */}
       </ul>
     </div>
   );
