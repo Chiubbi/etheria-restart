61548de1cfd5a318c9ff4385bacf7d48aff90c20
Chiubbi <chiubbicontact@gmail.com>
2025-05-05 22:21:42 +0200

    Update layout.js

--- pages/layout.js
+++ pages/layout.js
@@ -1,9 +1,21 @@
-// components/layout.js
-
 import styles from './layout.module.css';
 
 const Layout = ({ children }) => {
-  return <div className={styles.container}>{children}</div>;
+  return (
+    <div className={styles.container}>
+      <aside className={styles.sidebar}>
+        <h2>Catégories</h2>
+        <ul>
+          <li>Personnages</li>
+          <li>Tutoriels</li>
+          <li>Événements</li>
+        </ul>
+      </aside>
+      <main className={styles.mainContent}>
+        {children}
+      </main>
+    </div>
+  );
 };
 
 export default Layout;
