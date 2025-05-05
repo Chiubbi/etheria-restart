99cfe8541d813e70f537bcc8eae330279cc47065
Chiubbi <chiubbicontact@gmail.com>
2025-05-05 23:10:34 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
+// next.config.js
 module.exports = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: '/old-page',
+        destination: '/new-page',
+        permanent: true,
+      },
+    ];
+  },
+};
